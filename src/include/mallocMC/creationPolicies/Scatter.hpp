1e7eff1d627b504e0a1b2437e99eb29559738d57
Julian Lenz <j.lenz@hzdr.de>
2024-08-08 16:26:37 +0200

    Correct comment about protected Heap ctor

--- src/include/mallocMC/creationPolicies/Scatter.hpp
+++ src/include/mallocMC/creationPolicies/Scatter.hpp
@@ -198,8 +198,7 @@ namespace mallocMC::CreationPolicies::ScatterAlloc
         }
 
     protected:
-        // This class is supposed to be reinterpeted on a piece of raw memory and not instantiated directly. We set it
-        // protected, so we can still test stuff in the future easily.
+        // This class is supposed to be instantiated as a parent for the `DeviceAllocator`.
         Heap() = default;
     };
 
